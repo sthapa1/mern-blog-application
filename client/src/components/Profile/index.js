1bf34fe8572c8f8614b8afb3fc7f2b58ea44ceee
Sudip Thapa <sthapa@deerhold.org>
2021-11-16 16:22:14 +0545

    user profile created

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import CreatePost from './CreatePost/CreatePost'
-import UserCard from './UserCard'
-import {Modal} from 'react-bootstrap';
+import ProfileHeader from './ProfileHeader'
+import {Button, Card, Container, Modal} from 'react-bootstrap';
+import {useSelector} from 'react-redux';
+
 export default function Profile() {
+
+    const { user } = useSelector(state=>state.auth);
 
     const [openModal, setOpenModal] = React.useState(false);
 
@@ -13,14 +17,23 @@ export default function Profile() {
     }
 
     return (
-        <div className='container'>
-            <div className='d-flex justify-content-end mb-2 mt-2'>
-                <button onClick={handleCreatePostClick} className="btn btn-primary">Create post</button>
-            </div>
-            <UserCard />
-            <Modal show={openModal} onHide={handleClose}>
-                <CreatePost close={handleClose} />
-            </Modal>
-        </div>
+        // <div className='container'>
+        //     <div className='d-flex justify-content-end mb-2 mt-2'>
+        //         <button onClick={handleCreatePostClick} className="btn btn-primary">Create post</button>
+        //     </div>
+        //     <UserCard />
+        //     <Modal show={openModal} onHide={handleClose}>
+        //         <CreatePost close={handleClose} />
+        //     </Modal>
+        // </div>
+        <Container>
+            <Card className='p-4 w-100 d-flex justify-content-center'>
+                <ProfileHeader user={user || {}} />
+                <div className='d-flex justify-content-between align-items-center'>
+                    <h4 className='pt-4 text-muted'>My Posts</h4>
+                    <Button>Create Post</Button>
+                </div>
+            </Card>
+        </Container>
     )
 }
